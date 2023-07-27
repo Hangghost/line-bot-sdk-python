0944b68d123446f2c7589ad64822624d01aa460b
陳泓綸 <chenhunglun@chenhonglundeMacBook-Pro.local>
2023-07-27 19:38:36 +0800

    v1.2.5 modify response

--- app.py
+++ app.py
@@ -20,6 +20,11 @@ def linebot():
     try:
         access_token = 'EU16pZnitJZotJBmSSMxizmnT7TsmYrVI6GLIzDHJDWFHs/1IMKW3rUMB0pJc2razv8npE2l7LT9u47K1c7ytC4493F4w3U4U+8XbdI3rhV6ceOw0zP5LhOIz/1DB9Urm3SBadKi/I4naS+7+mxXsgdB04t89/1O/w1cDnyilFU='
         secret = 'fa5368b825917a6136d1f10fa753f1de'
+        openai.api_type = "azure"
+        openai.api_base = "https://herme-service.openai.azure.com/"
+        openai.api_version = "2023-03-15-preview"
+        openai.api_key = os.getenv("d6576c8d912946aeb070b712e1ca43d5")
+        
         line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
         handler = WebhookHandler(secret)                     # 確認 secret 是否正確
         signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
@@ -31,10 +36,6 @@ def linebot():
             ai_msg = msg[:6].lower()                         # 取出文字的前五個字元，轉換成小寫
             reply_msg = ""                                   # 預設回覆訊息
             if ai_msg == 'hi ai:':                                
-                openai.api_type = "azure"
-                openai.api_base = "https://herme-service.openai.azure.com/openai/deployments/GPT-35-turbo/chat/completions?api-version=2023-03-15-preview"
-                openai.api_version = "2023-03-15-preview"
-                openai.api_key = os.getenv("d6576c8d912946aeb070b712e1ca43d5")
                 response = openai.Completion.create(         
                     engine="GPT-35-turbo",
                     messages = [{"role":"system","content":"You are an Xbox customer support agent whose primary goal is to help users with issues they are experiencing with their Xbox devices. You are friendly and concise. You only provide factual answers to queries, and do not provide answers that are not related to Xbox."},{"role":"user","content":"How much is a PS5?"},{"role":"assistant","content":"I apologize, but I do not have information about the prices of other gaming devices such as the PS5. My primary focus is to assist with issues regarding Xbox devices. Is there a specific issue you are having with your Xbox device that I may be able to help with?"},{"role":"user","content":msg[6:]}],                          # 將第六個字元之後的訊息發送給 OpenAI
